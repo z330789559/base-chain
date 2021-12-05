5fb06d7d2a49373f287fe5c70da0f1a615696808
li kevin <libaozhong1984@gmail.com>
2021-12-06 02:23:06 +0800

    减少账户最低金额

--- ccm/runtime/src/lib.rs
+++ ccm/runtime/src/lib.rs
@@ -267,7 +267,7 @@ impl pallet_timestamp::Config for Runtime {
 }
 
 parameter_types! {
-    pub const ExistentialDeposit: u128 = 500 * MILLICENTS;
+    pub const ExistentialDeposit: u128 = 5 * MILLICENTS;
     // For weight estimation, we assume that the most locks on an individual account will be 50.
     // This number may need to be adjusted in the future if this assumption no longer holds true.
     pub const MaxLocks: u32 = 50;
